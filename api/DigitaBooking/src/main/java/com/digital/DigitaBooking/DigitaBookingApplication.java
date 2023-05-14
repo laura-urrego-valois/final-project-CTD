e2ce969a21d5e0beb696dedfbd069fb306beb035
Juan Pablo Narvaez <juanp.narvaez@gmail.com>
2023-05-14 14:58:44 -0500

    se verifica archivos de build

---
+++ api/DigitaBooking/src/main/java/com/digital/DigitaBooking/DigitaBookingApplication.java
@@ -0,0 +1,13 @@
+package com.digital.DigitaBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaBookingApplication.class, args);
+	}
+
+}
--- api/DigitalBooking/src/main/java/com/digital/DigitalBooking/Model/Dtos/Producto.java
+++
@@ -1,8 +0,0 @@
-package com.digital.DigitalBooking.Model.Dtos;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class Producto {
-
-}
