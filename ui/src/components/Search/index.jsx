60943071667092136fd7fcd6060c69d6460a1a6e
Jose Caicedo <jose.caicedo@outlook.com>
2023-05-15 00:13:37 -0500

    search section

--- ui/src/components/Search/index.jsx
+++ ui/src/components/Search/index.jsx
@@ -1,21 +1,25 @@
-import { Input } from '../Input'
-import './Search.css'
+import { Input } from "../../components/Input"
+import { Button } from '../Button'
 import Location from '../../assets/localizador.svg'
 import Calendar from '../../assets/calendar.svg'
+import './Search.css'
 
 export const Search = () => {
   return (
     <section className="search">
-      <h1>Busca ofertas de experiencia turística</h1>
+      <h1 className='search__title'>Busca ofertas de experiencia turística</h1>
       <form action="" className='search-form'>
         <Input
-          imageSrc={Location}
+          className="input-location"
+          iconSrc={Location}
           type="text"
           placeholder="¿A dónde vamos?" />
         <Input
-          imageSrc={Calendar}
-          type="text"
+          className="input-calendar"
+          iconSrc={Calendar}
+          type="date"
           placeholder="Check in - Check out" />
+        <Button type="primary">Buscar</Button>
       </form>
     </section>
   )
