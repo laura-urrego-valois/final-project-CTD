bf3a888b3b42f4e85769967aa82d00fab2100c7a
Jose Caicedo <jose.caicedo@outlook.com>
2023-05-16 18:38:22 -0500

    fix:change of footer position and size delimitation

--- ui/src/components/Footer/footer.jsx
+++ ui/src/components/Footer/footer.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
 import "./footer.css";
 
 const Footer = () => {
   return (
     <footer className='footer'>
-        <div>
-        <a rel="noreferrer" target="_blank"><img src="../src/assets/logodb.png"  alt='isologodigitalbooking'/></a>
+      <div>
+        <a rel="noreferrer" target="_blank"><img src="../src/assets/logodb.png" alt='isologodigitalbooking' /></a>
         <p>2023 © Digital Booking</p>
-        </div>
-          <div className='social-icons'>
-            <a className='social-media' href="https://www.facebook.com" rel="noreferrer" target="_blank"><img class= "icon" src="../src/assets/ico-facebook.png"  alt='ico-facebook'/></a>
-            <a className='social-media' href="https://www.instagram.com" rel="noreferrer" target="_blank"><img class= "icon" src="../src/assets/ico-instagram.png"  alt='ico-instagram'/></a>
-            <a className='social-media' href="https://www.tiktok.com" rel="noreferrer" target="_blank"><img class= "icon" src="../src/assets/ico-tiktok.png"  alt='ico-tiktok'/></a>
-            <a className='social-media' href="https://www.whatsapp.com" rel="noreferrer" target="_blank"><img class= "icon" src="../src/assets/ico-whatsapp.png"  alt='ico-whatsapp'/></a>
-          </div>
+      </div>
+      <div className='social-icons'>
+        <a className='social-media' href="https://www.facebook.com" rel="noreferrer" target="_blank">
+          <img className="icon" src="../src/assets/ico-facebook.png" alt='ico-facebook' />
+        </a>
+        <a className='social-media' href="https://www.instagram.com" rel="noreferrer" target="_blank">
+          <img className="icon" src="../src/assets/ico-instagram.png" alt='ico-instagram' />
+        </a>
+        <a className='social-media' href="https://www.tiktok.com" rel="noreferrer" target="_blank">
+          <img className="icon" src="../src/assets/ico-tiktok.png" alt='ico-tiktok' />
+        </a>
+        <a className='social-media' href="https://www.whatsapp.com" rel="noreferrer" target="_blank">
+          <img className="icon" src="../src/assets/ico-whatsapp.png" alt='ico-whatsapp' />
+        </a>
+      </div>
     </footer>
   )
 }
