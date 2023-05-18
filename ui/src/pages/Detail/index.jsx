3bdbd948d0fcd7aed9e2b18e8c75e63fd62481f6
Jose Caicedo <jose.caicedo@outlook.com>
2023-05-18 10:31:37 -0500

    product detail page layout

--- ui/src/pages/Detail/index.jsx
+++ ui/src/pages/Detail/index.jsx
@@ -1,11 +1,12 @@
-import { useParams } from "react-router-dom";
-import { Container } from "../../components/Container";
-
+import { Link, useParams } from "react-router-dom";
 import { useGlobalState } from "../../context";
+import { Button } from "../../components/Button";
+import './Detail.css'
 
 const Detail = () => {
 	let { id } = useParams();
 	const { state } = useGlobalState();
+	const { categories, tours } = state
 
 	// Descomentar cuando funcione endpoint get by id
 
@@ -18,14 +19,33 @@ const Detail = () => {
 	// 		});
 	// 	};
 
-	const itemDetail = state?.tours?.find(
-		(item) => parseInt(item.id_tour) === parseInt(id)
+	const itemDetail = tours?.find(
+		(item) => item.id == id
 	);
+	const getCategoryName = (categoryId) => {
+		const category = categories?.find((cat) => cat.id_categoria === categoryId);
+		return category ? category.nombre : "";
+	};
 
 	return (
-		<Container>
-			<h1>{itemDetail?.nombre}</h1>
-		</Container>
+		<article className="detail__container">
+			<div className="detail">
+				<h1 className="detail__title">{itemDetail.title}</h1>
+				<Link to="/">
+					<Button type="secondary">⬅</Button>
+				</Link>
+			</div>
+			<section className="detail__content">
+				<img className="detail__img" src={itemDetail.img} alt="" />
+				<div className="detail__info">
+					<div className="detail-value">
+						<h3 className="detail__price">{`Price: ${itemDetail.price} USD`}</h3>
+						<p className="detail__category">{getCategoryName(itemDetail.id_categoria)}</p>
+					</div>
+					<p className="detail__description">{itemDetail.description}</p>
+				</div>
+			</section>
+		</article>
 	);
 };
 
