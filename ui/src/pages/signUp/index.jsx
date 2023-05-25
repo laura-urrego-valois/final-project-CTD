5963f1a2b2da5e7057cca83f9b05eb94c1190a22
karvaroz <vargasriosk@gmail.com>
2023-05-24 23:39:29 -0500

    manejo de formularios agregar paquete

--- ui/src/pages/signUp/index.jsx
+++ ui/src/pages/signUp/index.jsx
@@ -1,35 +1,96 @@
 import Swal from "sweetalert2";
+import { useState } from "react";
 
-import { useForm } from "../../hooks/useForm";
 
 import { Container } from "../../components/Container"
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 export const SignUp = () => {
-    const { data, setData, handleChange } = useForm();
+    const [dataForm, setDataForm] = useState({
+        id: new Date().getTime()
+    });
+
+    const handleChange = ({ target }) => {
+        setDataForm({
+            ...dataForm,
+            [target.name]: target.value,
+        });
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         // await createItem(data)
-        Swal.fire("Login");
-        setData({});
+        // setData({});
         // navigate("/");
+        console.log(dataForm)
+        Swal.fire("Login");
 
     };
+
     return (
         <Container>
-
             <h1 className='form_title'>Crear Cuenta</h1>
             <form className="login__form" onSubmit={handleSubmit}>
-                <Input displayLabel="Nombre" label="name" type="text" onChange={handleChange} value={data?.name} required />
-                <Input displayLabel="Apellido" label="name" type="text" onChange={handleChange} value={data?.name} required />
-                <Input displayLabel="Correo Electronico" label="name" type="text" onChange={handleChange} value={data?.name} required />
-                <Input displayLabel="Contrasena" label="description" type="password" onChange={handleChange} value={data?.description} required />
-                <Input displayLabel="Confirmar Contrasena" label="description" type="password" onChange={handleChange} value={data?.description} required />
+                <Input
+                    displayLabel="Nombre"
+                    label="name"
+                    type="text"
+                    onChange={handleChange}
+                    value={dataForm?.name}
+                    errorMessage="no se"
+                    required
+                />
+                <Input
+                    displayLabel="Apellido"
+                    label="lastName"
+                    type="text"
+                    onChange={handleChange}
+                    value={dataForm?.lastName}
+                    required
+                />
+                <Input
+                    displayLabel="Correo Electronico"
+                    label="email"
+                    type="email"
+                    onChange={handleChange}
+                    value={dataForm?.email}
+                    required
+                />
+                <Input
+                    displayLabel="Contrasena"
+                    label="password"
+                    type="password"
+                    onChange={handleChange}
+                    value={dataForm?.password}
+                    required
+                />
+                <Input
+                    displayLabel="Confirmar Contrasena"
+                    label="confirmPassword"
+                    type="password"
+                    onChange={handleChange}
+                    value={dataForm?.confirmPassword}
+                    required
+                />
+                <label htmlFor="role">
+                    Rol
+                    <select
+                        value={dataForm?.role}
+                        onChange={handleChange}
+                        id="role"
+                        name="role">
+                        <option disabled hidden>Seleccione</option>
+                        <option
+                            value={1}
+                        >
+                            Cliente
+                        </option>
+                        <option value={2}>Administrador</option>
+                    </select>
+                </label>
                 <Button type="primary">Registrarse</Button>
             </form>
-
         </Container>
     )
 }
