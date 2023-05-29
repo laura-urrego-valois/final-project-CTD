3f61e5ca85a047cb136978acdf7164205111bd86
Laura <laura.urrego@hotmail.com>
2023-05-28 20:11:16 -0500

    Latest changes in SignIn and LogIn, JWT handling is also added.

--- api/DigitalBooking/src/main/java/com/digital/DigitaBooking/controllers/UserController.java
+++ api/DigitalBooking/src/main/java/com/digital/DigitaBooking/controllers/UserController.java
@@ -3,13 +3,25 @@ package com.digital.DigitaBooking.controllers;
 import com.digital.DigitaBooking.models.dtos.PageResponseDTO;
 import com.digital.DigitaBooking.models.dtos.UserDTO;
 import com.digital.DigitaBooking.models.dtos.UserSignUp;
+import com.digital.DigitaBooking.models.dtos.UserPageDTO;
 import com.digital.DigitaBooking.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -22,10 +34,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @PostMapping
     public ResponseEntity<?> saveUser(@RequestBody UserSignUp userSignUp) {
         userService.registerUser(userSignUp);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserDetails> getUserByUsername(@PathVariable("username") String username) {
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            return ResponseEntity.ok(userDetails);
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping(path = "/{id}")
@@ -33,14 +58,27 @@ public class UserController {
         return userService.getUser(id);
     }
 
-    /*
+    // Método de controlador utilizando anotaciones de Swagger y Spring Framework.
+    @Operation(security = {@SecurityRequirement(name = "bearer-key")}, summary = "Retrieve a paginated list of users",
+            parameters = {@Parameter(in = ParameterIn.QUERY, name = "page", description = "Page"),
+                    @Parameter(in = ParameterIn.QUERY, name = "size", description = "Size")},
+            responses = {@ApiResponse(responseCode = "200", description = "Request processed successfully",
+                    content = @Content(mediaType = "application(json", schema = @Schema(implementation = UserPageDTO.class)))})
+
+    // * security: Especifica los requisitos de seguridad para la operación. En este caso,
+    // se requiere el uso de un "bearer-key" para autenticación.
+    // Tenemos 2 parámetros definidos utilizando la anotación @Parameter ("page" y "size").
+    // * in: Indica la ubicación del parámetro. En este caso, ambos parámetros son de tipo
+    // QUERY, lo que significa que se enviarán en la URL como parámetros de consulta.
+    // En responses tenemos un content que en nuestro caso se establece como
+    // "application/json" para indicar que la respuesta será en formato JSON.
+    // Por otro lado, tenemos schema que define la estructura del objeto de respuesta.
+    // En este caso, se utiliza la clase UserPageDTO para representar la lista paginada
+    // de usuarios.
+
     @GetMapping
-    public Collection<UserDTO> getUsers() {
-        return userService.getUsers();
+    public PageResponseDTO<UserDTO> getUsers(@PageableDefault(size = 10, page = 0) @ParameterObject Pageable pageable) {
+        return userService.getUsers(pageable);
     }
-
-    //En proceso de modificación.
-     */
-
-
+    // Este método obtiene una lista paginada de usuarios y la devuelve como una respuesta.
 }
