da3a8d49317ed7ab27ca378dc7a0b3c88cd4c4fc
Jose Caicedo <jose.caicedo@outlook.com>
2023-06-03 03:14:42 +0000

    Revert "Merge branch 'main' into 'dev'"
    This reverts commit 6952122b4e7d20a24479956ecb87fc43db11b439

--- ui/src/routes/index.jsx
+++ ui/src/routes/index.jsx
@@ -6,16 +6,13 @@ import Detail from "../pages/Detail";
 import { ProductList } from "../pages/productList";
 import { Admin } from "../pages/admin";
 import { Register } from "../pages/register";
-
-import { Login } from "../pages/login";
-import { SignUp } from "../pages/signUp";
-import { User } from "../pages/user";
-
 import { Success } from "../pages/success";
 import { CategoryList } from "../pages/categoryList";
 import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
-
+import { Login } from "../pages/login";
+import { SignUp } from "../pages/signUp";
+import { User } from "../pages/user";
 
 
 export const AppRoutes = () => {
@@ -44,8 +41,16 @@ export const AppRoutes = () => {
 					element={<ProductList />}
 				/>
 				<Route
+					path="/category-list"
+					element={<CategoryList />}
+				/>
+				<Route
 					path="/register"
 					element={<Register />}
+				/>
+				<Route
+					path="/register-success"
+					element={<Success />}
 				/>
 				<Route
 					path="/login"
@@ -54,14 +59,6 @@ export const AppRoutes = () => {
 				<Route
 					path="/signup"
 					element={<SignUp />}
-				/>
-				<Route
-					path="/register-success"
-					element={<Success />}
-				/>
-				<Route
-					path="/category-list"
-					element={<CategoryList />}
 				/>
 				<Route
 					path="*"
