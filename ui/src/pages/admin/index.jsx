92fb8276e318190efe1d71954920aa3864ccd953
Jose Caicedo <jose.caicedo@outlook.com>
2023-06-07 18:16:23 -0500

    sidebar admin

--- ui/src/pages/admin/index.jsx
+++ ui/src/pages/admin/index.jsx
@@ -1,22 +1,13 @@
-import { Button } from "../../components/Button"
 import { Container } from "../../components/Container"
-import { useNavigate } from 'react-router-dom';
+import { SidebarLayout } from "../../layout/sidebar";
 
 export const Admin = () => {
-  const navigate = useNavigate();
 
-  const redirectToProductList = () => {
-    navigate('/product-list');
-  };
-
-  const redirectToRegister = () => {
-    navigate('/register');
-  };
   return (
     <Container>
-      <h1>Zona Admin</h1>
-      <Button onClick={redirectToProductList}>Lista de producto</Button>
-      <Button onClick={redirectToRegister}>Registrar</Button>
+      <SidebarLayout>
+        <h1>Zona Admin</h1>
+      </SidebarLayout>
     </Container>
   )
 }
--- ui/src/pages/productList/index.jsx
+++ ui/src/pages/productList/index.jsx
@@ -1,12 +1,15 @@
 import { Container } from "../../components/Container"
 import { ListProduct } from "../../components/ListProduct"
+import { SidebarLayout } from "../../layout/sidebar"
 
 export const ProductList = () => {
   return (
     <>
       <Container>
-        <h1>Lista de productos</h1>
-        <ListProduct />
+        <SidebarLayout>
+          <h1>Lista de productos</h1>
+          <ListProduct />
+        </SidebarLayout>
       </Container>
     </>
   )
