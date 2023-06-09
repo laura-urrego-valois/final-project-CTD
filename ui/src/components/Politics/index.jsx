8fea75759a6489bc0c70c742bd9992ea8777f645
Jose Caicedo <jose.caicedo@outlook.com>
2023-06-08 20:01:25 -0500

    section layout

---
+++ ui/src/components/Politics/index.jsx
@@ -0,0 +1,23 @@
+import './Politics.css'
+
+export const Politics = () => {
+  return (
+    <section className='politics'>
+      <h2 className='politics__title'>Que Tenés que saber</h2>
+      <div className='politics__container'>
+        <article className='politics__item'>
+          <h4>Normas de la casa</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima delectus impedit ratione beatae repellendus. Eveniet id in laudantium culpa hic, perferendis mollitia nesciunt assumenda? Accusamus error ut dolorem dolorum!</p>
+        </article>
+        <article className='politics__item'>
+          <h4>Salud y Seguridad</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima delectus impedit ratione beatae repellendus. Eveniet id in laudantium culpa hic, perferendis mollitia nesciunt assumenda? Accusamus error ut dolorem dolorum!</p>
+        </article>
+        <article className='politics__item'>
+          <h4>Politíca de cancelación</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima delectus impedit ratione beatae repellendus. Eveniet id in laudantium culpa hic, perferendis mollitia nesciunt assumenda? Accusamus error ut dolorem dolorum!</p>
+        </article>
+      </div>
+    </section>
+  )
+}
