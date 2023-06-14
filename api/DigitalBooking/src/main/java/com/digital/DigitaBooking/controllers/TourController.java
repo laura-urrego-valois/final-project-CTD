bfae510ac77928e8e39ea9cf387d1dc002eac966
Juan Pablo Narvaez <juanp.narvaez@gmail.com>
2023-06-13 23:39:26 -0500

    se agrega logica para subida de imagenes

--- api/DigitalBooking/src/main/java/com/digital/DigitaBooking/controllers/TourController.java
+++ api/DigitalBooking/src/main/java/com/digital/DigitaBooking/controllers/TourController.java
@@ -1,15 +1,28 @@
 package com.digital.DigitaBooking.controllers;
 
+import com.digital.DigitaBooking.AWSS3Service;
+import com.digital.DigitaBooking.models.dtos.ImageDTO;
+import com.digital.DigitaBooking.models.dtos.ImageLoaderDTO;
 import com.digital.DigitaBooking.models.dtos.TourDTO;
+import com.digital.DigitaBooking.models.dtos.UserDTO;
+import com.digital.DigitaBooking.service.impl.AWSS3ServiceImpl;
+import com.digital.DigitaBooking.services.impl.ImageService;
 import com.digital.DigitaBooking.services.impl.TourService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.catalina.mapper.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 @CrossOrigin(origins="*")
 @RestController
@@ -19,12 +32,58 @@ public class TourController {
     @Autowired
     private TourService tourService;
 
+    @Autowired
+    private AWSS3ServiceImpl awss3Service;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    private ImageDTO imageDTO;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> saveTour(@RequestBody TourDTO tourDTO) {
         tourService.saveTour(tourDTO);
         return ResponseEntity.ok(HttpStatus.OK);
 
+    }
+
+    @PostMapping(path = "/load_image")
+    public ResponseEntity<HttpStatus> loadImage(@RequestPart(value="files") List<MultipartFile> imagenes,
+    @RequestPart(value="Tour") String tourString) throws IOException {
+
+        List<String> imagesURL = new ArrayList<>();
+        TourDTO tourDTO = mapper.readValue(tourString,TourDTO.class);
+        try {
+            tourService.saveTour(tourDTO);
+//            tourService.getTour()
+            for (MultipartFile image: imagenes){
+                File mainFile = new File(image.getOriginalFilename());
+                String newFileName = System.currentTimeMillis() + "_" + mainFile.getName();
+                awss3Service.uploadFile(image);
+                imageDTO.setImageTitle(mainFile.getName());
+                imageDTO.setImageUrl(awss3Service.generateUrl(newFileName).replaceFirst("/[0-9]+_", "/_"));
+//                imageDTO.setTour(tourService.getTour());
+                imageService.saveImage(imageDTO);
+
+                imagesURL.add(awss3Service.generateUrl(newFileName).replaceFirst("/[0-9]+_", "/_"));
+
+
+//                public void addImagesToProduct(Product newProduct, List<String> imagesURLs){
+//                    List<ProductImageEntity> images = imagesURLs.stream().map(url -> new ProductImageEntity(null, url, newProduct)).toList();
+//                    productImageRepository.saveAll(images);
+//                }
+            }
+
+
+        }catch (Exception e){
+            return ResponseEntity.ok(HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
--- api/DigitalBooking/src/main/java/com/digital/DigitaBooking/models/dtos/TourDTO.java
+++ api/DigitalBooking/src/main/java/com/digital/DigitaBooking/models/dtos/TourDTO.java
@@ -2,11 +2,13 @@ package com.digital.DigitaBooking.models.dtos;
 
 import com.digital.DigitaBooking.models.entities.Category;
 import com.digital.DigitaBooking.models.entities.Feature;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import java.util.Set;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TourDTO {
 
     private Long id;
@@ -21,4 +23,5 @@ public class TourDTO {
     private Category category;
     private Set<Feature> features;
     private Set<Long> featuresId;
+
 }
