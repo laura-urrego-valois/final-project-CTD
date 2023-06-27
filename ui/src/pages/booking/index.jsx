21a8ab952212af65c34a50bf66f14d2e41c9a2f2
karvaroz <vargasriosk@gmail.com>
2023-06-27 18:08:42 -0500

    trabajando en reserva

---
+++ ui/src/pages/booking/index.jsx
@@ -0,0 +1,120 @@
+import { Link, useNavigate } from "react-router-dom"
+import { Container } from "../../components/Container"
+import { Politics } from "../../components/Politics"
+import { BsFillArrowLeftCircleFill } from "react-icons/bs"
+import "./booking.css"
+import { Input } from "../../components/Input"
+import { DatesPicker } from "../../components/DatesPicker"
+import { Button } from "../../components/Button"
+import { useEffect, useState } from "react"
+import { useGlobalState } from "../../context"
+
+export const BookingPage = () => {
+  const { user, createReservation } = useGlobalState()
+  const [bookingDetail, setBookingDetail] = useState({})
+  const navigate = useNavigate()
+  useEffect(() => {
+    const booking = JSON.parse(localStorage.getItem("booking"))
+    if (booking) {
+      setBookingDetail(booking)
+    }
+  }, [])
+
+  const handleBookingClick = async (e) => {
+    e.preventDefault()
+    const response = await createReservation({
+      initialDate: bookingDetail?.initialDate,
+      finalDate: bookingDetail?.finalDate,
+      startTime: bookingDetail?.startTime,
+      idTour: parseInt(bookingDetail?.idTour),
+      idUser: parseInt(user?.id),
+    })
+
+    console.log({
+      initialDate: bookingDetail?.initialDate,
+      finalDate: bookingDetail?.finalDate,
+      startTime: bookingDetail?.startTime,
+      idTour: parseInt(bookingDetail?.idTour),
+      idUser: parseInt(user?.id),
+    })
+
+    if (response) {
+      navigate("/booking-success")
+    }
+    navigate("/booking-failure")
+  }
+
+  return (
+    <Container>
+      <div className="detail">
+        <div>
+          <h1 className="detail__title">Detalle Reserva</h1>
+        </div>
+        <Link className="detail__icon" to="/">
+          <BsFillArrowLeftCircleFill />
+        </Link>
+      </div>
+      <section className="booking__container">
+        <div className="booking__right">
+          <form className="booking__form">
+            <h3>Tus datos</h3>
+            <Input
+              displayLabel="Nombre"
+              label="userFirstName"
+              type="text"
+              value={user?.userFirstName}
+              disabled
+            />
+            <Input
+              displayLabel="Apellido"
+              label="userLastName"
+              type="text"
+              value={user?.userLastName}
+              disabled
+            />
+            <Input
+              displayLabel="Email"
+              label="userName"
+              type="text"
+              value={user?.username}
+              disabled
+            />
+          </form>
+          <div className="booking__calendar">
+            <DatesPicker />
+          </div>
+          <form className="booking__time">
+            <h3>Tu hora</h3>
+            <Input displayLabel="Hora de Inicio" label="time" type="time" />
+          </form>
+        </div>
+        <div className="booking__detail">
+          <h4>Detalle de la reserva</h4>
+          <img src={bookingDetail?.tourImage} />
+          <h4>{bookingDetail?.tourName}</h4>
+          <p>Precio: ${bookingDetail?.tourPrice}USD</p>
+          <form>
+            <Input
+              displayLabel="Fecha Inicio"
+              label="initialDate"
+              value={bookingDetail?.initialDate}
+              type="text"
+              disabled
+            />
+            <Input
+              displayLabel="Fecha Final"
+              label="finalDate"
+              value={bookingDetail?.finalDate}
+              type="text"
+              disabled
+            />
+            <Button type="primary" onClick={(e) => handleBookingClick(e)}>
+              Reservar
+            </Button>
+          </form>
+        </div>
+      </section>
+      <Politics />
+    </Container>
+  )
+}
