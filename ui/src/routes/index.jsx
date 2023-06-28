90d84bc950e51080bc314727e22aa5a1f4034cc7
karvaroz <vargasriosk@gmail.com>
2023-06-27 21:05:42 -0500

    rutas de reservas

--- ui/src/routes/index.jsx
+++ ui/src/routes/index.jsx
@@ -21,8 +21,8 @@ import ProtectedRoutes from "./ProtectedRoutes"
 import { CountryList } from "../pages/countryList"
 import { UserList } from "../pages/userList"
 import { useGlobalState } from "../context"
-import Booking from "../pages/Booking"
 import { Favorites } from "../pages/favorites"
+import { BookingPage } from "../pages/booking"
 
 export const AppRoutes = () => {
   const { user } = useGlobalState()
@@ -41,16 +41,20 @@ export const AppRoutes = () => {
           <Route path="/category-list" element={<CategoryList />} />
           <Route path="/country-list" element={<CountryList />} />
           <Route path="/user-list" element={<UserList />} />
+
+          <Route path="/booking/:id" element={<BookingPage />} />
+          <Route path="/booking-success" element={<BookingSuccessful />} />
+          <Route path="/booking-failure" element={<BookingFailure />} />
+          <Route
+            path="/history-reservation"
+            element={<HistoryReservations />}
+          />
         </Route>
 
         <Route path="/register" element={<Register />} />
         <Route path="/register-success" element={<Success />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-
-        <Route path="/booking-success" element={<BookingSuccessful />} />
-        <Route path="/booking-failure" element={<BookingFailure />} />
-        <Route path="/history-reservation" element={<HistoryReservations />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
